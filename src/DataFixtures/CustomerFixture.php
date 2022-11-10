abb3d661dad49ad74c40e007e12244b6575c13f2
clementPere <clement.pere.dev@gmail.com>
2022-11-10 01:15:40 +0100

    add Jwt authentication

--- src/DataFixtures/CustomerFixture.php
+++ src/DataFixtures/CustomerFixture.php
@@ -2,22 +2,33 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Customer;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use Faker;
+use App\Entity\Customer;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class CustomerFixture extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
         for ($i = 0; $i < 10; $i++) {
             $customer = new Customer;
             $company = $faker->company;
+            $role = ["ROLE_USER"];
+            if ($i === 0) {
+                $company = "admin";
+                $role = ["ROLE_ADMIN"];
+            }
+            $plaintextPassword = str_replace(" ", "", $company) . "1234";
             $customer->setName($company)
                 ->setEmail(str_replace(" ", "", $company) . "@gmail.com")
-                ->setPassword(str_replace(" ", "", $company) . "1234");
+                ->setPassword($this->passwordHasher->hashPassword($customer, $plaintextPassword))
+                ->setRoles($role);
             $manager->persist($customer);
         }
         $manager->flush();
--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -6,9 +6,11 @@ use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
-class Customer
+class Customer implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -18,8 +20,11 @@ class Customer
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private ?string $email = null;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
@@ -61,6 +66,35 @@ class Customer
         return $this;
     }
 
+    /**
+     * The public representation of the user (e.g. a username, an email address, etc.)
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
     public function getPassword(): ?string
     {
         return $this->password;
@@ -71,6 +105,26 @@ class Customer
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     /**
